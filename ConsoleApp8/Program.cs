286f1eb6d046f28126aa8086f23de60fcf85cbbe
Oscar Jair Cuadros Nuñez <N00377634@upn.pe>
2023-09-02 10:43:39 -0500

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Por favor ingrese número de personas");
+int numeroP = int.Parse(Console.ReadLine());
+float TOT = 0f;
+if (numeroP > 300)
+{
+    TOT = numeroP * 75;
+}
+else
+{
+    if (numeroP > 200)
+    {
+        TOT = numeroP * 85;
+    }
+    else
+    {
+        TOT = numeroP * 95;
+    }
+}
+Console.WriteLine("El total vendría a ser : " + TOT);
